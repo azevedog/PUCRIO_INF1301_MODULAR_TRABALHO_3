8b10a40717dc0a206a8f780fe376c7697204fcbb
azevedog <gustavobhazevedo@gmail.com>
2016-11-05 13:39:29 -0200

    Inicio da infra do modulo jogo

---
+++ TRAB3-4/JOGO.C
@@ -0,0 +1,53 @@
+/***************************************************************************
+*  $MCI Módulo de implementação: JOG  Jogo generico 
+*
+*  Arquivo gerado:              JOGO.c
+*  Letras identificadoras:      JOG
+*
+*  Nome da base de software:    OpenJogos
+*
+*  Projeto: MODULAR_T2
+*  Gestor:  EAMCEMP LTDA
+*  Autores: MCS e GBHA
+*
+*  $HA Histórico de evolução:
+*     Versão  Autor    Data     Observações
+*      1       gbha   5/NOV/2016 início desenvolvimento
+*
+***************************************************************************/
+
+#include   <stdio.h>
+#include   <string.h>
+#include   <memory.h>
+#include   <malloc.h>
+#include   <assert.h>
+#include	"TABULEIRO.h"
+#include	"LISTA.h"
+#include	"PECA.h"
+
+/***********************************************************************
+*
+*  $TC Tipo de dados: JOG Descritor do Jogo
+*
+*
+***********************************************************************/
+
+   typedef struct JOG_tagJogo {
+   
+				/* Numero  */
+			   
+   } JOG_tpJogo ;
+
+/***** Protótipos das funções encapuladas no módulo *****/
+	
+	
+
+/*****  Código das funções exportadas pelo módulo  *****/
+
+
+/*****  Código das funções encapsuladas no módulo  *****/
+   
+		
+
+/********** Fim do módulo de implementação: JOG  Jogo generico **********/
+
