3f14e8844f01f8296fd77a039e8c4b714d62f5e6
azevedog <gustavobhazevedo@gmail.com>
2016-11-15 09:33:28 -0200

    Desenvolvimento do modulo jogo

--- TRAB3-4/JOGO.C
+++ TRAB3-4/JOGO.C
@@ -26,6 +26,11 @@
 #include	"TABULEIRO.h"
 #include	"LISTA.h"
 
+#define LINHAS 8
+#define COLUNAS 8
+
+TAB_tppTabuleiro tabuleiro;
+
 /***********************************************************************
 *
 *  $TC Tipo de dados: JOG Descritor do Jogo
@@ -41,16 +46,83 @@
 
 /***** Protótipos das funções encapuladas no módulo *****/
 	
-	static int converteColuna(char coluna);	
+	static int converteColuna(char coluna);
+	
+	static int converterLinha(int linha);
+	
+	static void initTabuleiro();
 
 /*****  Código das funções exportadas pelo módulo  *****/
 
+/***********************************************************************
+*
+*  $FC Função: JOG  -ImprimirTabuleiro
+*
+***********************************************************************/
+
+   JOG_tpCondRet JOG_ImprimirPosicoes(){
+   
+	int lin, col;
+	initTabuleiro();
+	
+	
+	for(lin = 0; lin< LINHAS; lin++){
+		printf("\n%d ", (LINHAS - lin));
+	
+		for(col = 0; col< COLUNAS; col++){
+			if(TAB_ObterPeca(int linhaInicial, int colunaInicial, char** id,
+				TAB_tppTabuleiro tabuleiro) == TAB_CondRetOK){
+				
+			}else{
+				printf("ERROR!!!\n");
+				return JOG_CondRetErro;
+			
+			}
+			
+		}
+	}
+		
+	/* PrintLetters */
+   
+   return JOG_CondRetOK
+   } /* Fim função: JOG  -ImprimirTabuleiro*/	
 
 /*****  Código das funções encapsuladas no módulo  *****/
+
+/***********************************************************************
+*
+*  $FC Função: JOG  -Inicia o tabuleiro
+*
+***********************************************************************/
+
+   void initTabuleiro(){
+   
+		tabuleiro = *((TAB_tppTabuleiro*) malloc(sizeof(TAB_tppTabuleiro)));
+		if(tabuleiro == NULL) return JOG_CondRetErro;
+		if(TAB_CriarTabuleiro(COLUNAS, LINHAS, &tabuleiro) != TAB_CondRetOK) return JOG_CondRetErro;
+		
+	
+	}/* Fim função: JOG  -Inicia o tabuleiro*/	
+
+
+/***********************************************************************
+*
+*  $FC Função: JOG  -Converte coordenada da linha
+*
+***********************************************************************/
+
+   int converteLinha(int linha){
+		/* Ajuste das coordenadas 1 based para a matriz interna 0 based, invertendo o
+		 * ponto (0,0)
+		 */
+		return LINHAS - linha
+   
+	}/* Fim função: JOG  -Converte coordenada da linha*/		
+
    
 /***********************************************************************
 *
-*  $FC Função: TAB  -Converte coordenada da coluna
+*  $FC Função: JOG  -Converte coordenada da coluna
 *
 ***********************************************************************/
 
@@ -68,7 +140,7 @@
 			dist = coluna - 'A';
 		}
 		return dist;
-	}/* Fim função: TAB  -Converte coordenada da coluna*/			
+	}/* Fim função: JOG  -Converte coordenada da coluna*/			
 
 /********** Fim do módulo de implementação: JOG  Jogo generico **********/
 
